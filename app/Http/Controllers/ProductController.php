f707835f7de5d0726b84d8a4b550808c12164a29
khrstn <khristine.sophia@gmail.com>
2024-06-27 21:08:28 +0800

    added update and destroy method in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,4 +24,25 @@ class ProductController extends Controller
 
         return Product::create($request->all());
     }
+
+    public function update(Request $request, $id){
+        $product = Product::findOrFail($id);
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric|between:0,999999.99',
+        ]);
+
+        $product->update($request->all());
+
+        return $product;
+    }
+
+    public function destroy(Request $request, $id){
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return response()->json(['message' => 'Product deleted successfully']);
+    }
 }
