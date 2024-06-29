9ee4f59d82761cd742b65c3a5654ec8b09efb702
khrstn <khristine.sophia@gmail.com>
2024-06-29 18:34:28 +0800

    implemented bootstrap classes in index and show View

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -3,12 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Product Details</title>
 </head>
 <body>
-    <h1>{{ $product->name }}</h1>
-    <p>{{ $product->description }}</p>
-    <p>Price: ${{ $product->price }}</p>
-    <a href="{{ route('products.index') }}">Back to Products</a>
+    <div class="container mt-4">
+        <h1 class="mb-4">{{ $product->name }}</h1>
+        <p class="lead">{{ $product->description }}</p>
+        <p class="h5">Price: ${{ $product->price }}</p>
+        <a href="{{ route('products.index') }}" class="btn btn-secondary mt-3">Back to Products</a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
